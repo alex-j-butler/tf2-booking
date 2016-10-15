bec12259f1c0dc9271e355aa68757bb94a666465
Alex Butler <a.jbutler019@gmail.com>
2016-10-15 18:51:15 +1000

    Remove old code in servers.go

--- servers.go
+++ servers.go
@@ -2,27 +2,6 @@ package main
 
 import ()
 
-/*
-type ServerConfig struct {
-	Name    string
-	Path    string
-	Address string
-}
-
-var Servers []Server
-
-func SetupServers() {
-	for i := 0; i < len(Conf.Servers); i++ {
-		s := Conf.Servers[i]
-		Servers = append(Servers, Server{
-			Name:    s.Name,
-			Path:    s.Path,
-			Address: s.Address,
-		})
-	}
-}
-*/
-
 func GetAvailableServer() *Server {
 	for i := 0; i < len(Conf.Servers); i++ {
 		if Conf.Servers[i].IsAvailable() {
