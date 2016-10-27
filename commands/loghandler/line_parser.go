581eb6e2a4bfac7efecb8fc665728c0dbe55eafb
Alex Butler <a.jbutler019@gmail.com>
2016-10-27 23:05:36 +1000

    Fix regexp parsing.

--- commands/loghandler/line_parser.go
+++ commands/loghandler/line_parser.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ParseLine(data string) (string, error) {
-	regex, err := regexp.Compile("\"(.+)<(\\d+)><(.+)><(Blue|Red|Unassigned|Spectator)>\" say \"(.+)\"")
+	regex, err := regexp.CompilePOSIX("\"(.+)<(\\d+)><(.+)><(Blue|Red|Unassigned|Spectator)>\" say \"(.+)\"")
 
 	if err != nil {
 		return "", err
@@ -16,7 +16,9 @@ func ParseLine(data string) (string, error) {
 	matches := regex.FindAllString(data, -1)
 
 	if len(matches) > 0 {
-		log.Println(matches)
+		for _, match := range matches {
+			log.Println(match)
+		}
 		// log.Println(fmt.Sprintf("User %s (%s): %s", matches[0], matches[2], matches[4]))
 
 		return "", nil
