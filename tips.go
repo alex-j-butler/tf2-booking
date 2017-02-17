ea69138c387737444d013070b1d1bd02a8a9e728
Alex Butler <a.jbutler019@gmail.com>
2017-02-17 21:26:49 +1000

    Fix panic when no tips have been added in the config file.

--- tips.go
+++ tips.go
@@ -8,8 +8,12 @@ import (
 )
 
 func ChooseRandomTip() string {
-	// Set the random seed (this doesn't need to be secure since we're just using it for a tip message).
-	rand.Seed(time.Now().UTC().UnixNano())
+	if length := len(config.Conf.Tips); length > 0 {
+		// Set the random seed (this doesn't need to be secure since we're just using it for a tip message).
+		rand.Seed(time.Now().UTC().UnixNano())
 
-	return config.Conf.Tips[rand.Intn(len(config.Conf.Tips))]
+		return config.Conf.Tips[rand.Intn(len(config.Conf.Tips))]
+	}
+
+	return "No tip messages in config file."
 }
