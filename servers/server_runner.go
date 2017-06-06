b4c2d7824f174433c766639302003e59ee9835e7
Alex Butler <a.jbutler019@gmail.com>
2017-06-06 20:41:15 +1000

    Properly commit previous changes

--- servers/server_runner.go
+++ servers/server_runner.go
@@ -11,13 +11,13 @@ import (
 	"alex-j-butler.com/tf2-booking/models"
 )
 
-type BookingAPIServerRunner struct {
+type ServerRunner struct {
 	APIClient     *client.Client
 	cachedServers map[string]*client.RPCServer
 }
 
-func NewBookingAPIRunner(apiClient *client.Client) *BookingAPIServerRunner {
-	return &BookingAPIServerRunner{
+func NewRunner(apiClient *client.Client) *ServerRunner {
+	return &ServerRunner{
 		APIClient:     apiClient,
 		cachedServers: make(map[string]*client.RPCServer),
 	}
@@ -32,7 +32,7 @@ const (
 
 var src = rand.NewSource(time.Now().UnixNano())
 
-func (br BookingAPIServerRunner) generatePassword() string {
+func (sr ServerRunner) generatePassword() string {
 	n := 10
 	b := make([]byte, n)
 
@@ -51,71 +51,71 @@ func (br BookingAPIServerRunner) generatePassword() string {
 	return string(b)
 }
 
-func (b BookingAPIServerRunner) getServer(uuid string) (*client.RPCServer, error) {
-	if server, ok := b.cachedServers[uuid]; ok {
+func (sr ServerRunner) getServer(uuid string) (*client.RPCServer, error) {
+	if server, ok := sr.cachedServers[uuid]; ok {
 		return server, nil
 	}
 
-	server, err := b.APIClient.GetServer(uuid)
+	server, err := sr.APIClient.GetServer(uuid)
 	if err != nil {
 		return nil, err
 	}
-	b.cachedServers[uuid] = &server
+	sr.cachedServers[uuid] = &server
 	return &server, nil
 }
 
-func (b BookingAPIServerRunner) Setup(server *Server) (rconPassword string, srvPassword string, err error) {
+func (sr ServerRunner) Setup(server *Server) (rconPassword string, srvPassword string, err error) {
 	// Generate an RCON and server password.
-	rconPassword = b.generatePassword()
-	srvPassword = b.generatePassword()
+	rconPassword = sr.generatePassword()
+	srvPassword = sr.generatePassword()
 
 	// Retrieve the API server instance from the API client.
-	apiServer, err := b.getServer(server.UUID)
+	apiServer, err := sr.getServer(server.UUID)
 	if err != nil {
 		return "", "", err
 	}
 
 	// Set the password on the server.
-	err = apiServer.SetPassword(b.APIClient, rconPassword, srvPassword)
+	err = apiServer.SetPassword(sr.APIClient, rconPassword, srvPassword)
 
 	return rconPassword, srvPassword, err
 }
 
-func (b BookingAPIServerRunner) Start(server *Server) error {
+func (sr ServerRunner) Start(server *Server) error {
 	// Retrieve the API server instance from the API client.
-	apiServer, err := b.getServer(server.UUID)
+	apiServer, err := sr.getServer(server.UUID)
 	if err != nil {
 		return err
 	}
 
 	// Start the server.
-	err = apiServer.Start(b.APIClient)
+	err = apiServer.Start(sr.APIClient)
 
 	return err
 }
 
-func (b BookingAPIServerRunner) Stop(server *Server) error {
+func (sr ServerRunner) Stop(server *Server) error {
 	// Retrieve the API server instance from the API client.
-	apiServer, err := b.getServer(server.UUID)
+	apiServer, err := sr.getServer(server.UUID)
 	if err != nil {
 		return err
 	}
 
 	// Stop the server.
-	err = apiServer.Stop(b.APIClient)
+	err = apiServer.Stop(sr.APIClient)
 
 	return err
 }
 
-func (b BookingAPIServerRunner) UploadSTV(server *Server) ([]models.Demo, error) {
+func (sr ServerRunner) UploadSTV(server *Server) ([]models.Demo, error) {
 	// Retrieve the API server instance from the API client.
-	apiServer, err := b.getServer(server.UUID)
+	apiServer, err := sr.getServer(server.UUID)
 	if err != nil {
 		return nil, err
 	}
 
 	// Upload demos.
-	demoURLs, err := apiServer.UploadDemos(b.APIClient)
+	demoURLs, err := apiServer.UploadDemos(sr.APIClient)
 	if err != nil {
 		return nil, err
 	}
@@ -128,33 +128,33 @@ func (b BookingAPIServerRunner) UploadSTV(server *Server) ([]models.Demo, error)
 	return demos, nil
 }
 
-func (b BookingAPIServerRunner) SendCommand(server *Server, command string) error {
+func (sr ServerRunner) SendCommand(server *Server, command string) error {
 	// Retrieve the API server instance from the API client.
-	apiServer, err := b.getServer(server.UUID)
+	apiServer, err := sr.getServer(server.UUID)
 	if err != nil {
 		return err
 	}
 
 	// Send the command.
-	err = apiServer.SendCommand(b.APIClient, command)
+	err = apiServer.SendCommand(sr.APIClient, command)
 
 	return err
 }
 
-func (b BookingAPIServerRunner) Console(server *Server, lines int) ([]string, error) {
+func (sr ServerRunner) Console(server *Server, lines int) ([]string, error) {
 	// Retrieve the API server instance from the API client.
-	apiServer, err := b.getServer(server.UUID)
+	apiServer, err := sr.getServer(server.UUID)
 	if err != nil {
 		return nil, err
 	}
-	consoleLines, err := apiServer.Console(b.APIClient, lines)
+	consoleLines, err := apiServer.Console(sr.APIClient, lines)
 
 	return consoleLines, err
 }
 
-func (b BookingAPIServerRunner) IsAvailable(server *Server) bool {
+func (sr ServerRunner) IsAvailable(server *Server) bool {
 	// Attempt to request the server information, if it fails, the server is unavailable.
-	_, err := b.APIClient.GetServer(server.UUID)
+	_, err := sr.APIClient.GetServer(server.UUID)
 	if err != nil {
 		// Unavailable!
 		return false
@@ -163,8 +163,8 @@ func (b BookingAPIServerRunner) IsAvailable(server *Server) bool {
 	return true
 }
 
-func (b BookingAPIServerRunner) IsBooked(server *Server) bool {
-	apiServer, err := b.APIClient.GetServer(server.UUID)
+func (sr ServerRunner) IsBooked(server *Server) bool {
+	apiServer, err := sr.APIClient.GetServer(server.UUID)
 	if err != nil {
 		return false
 	}
